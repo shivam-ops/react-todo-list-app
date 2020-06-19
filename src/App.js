78a43444ef838a6132265faa31081997bf591266
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-19 09:28:23 +0530

    updated styles, input and button elements

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key:''
+      }
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+    this.additem = this.additem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    });
+  }
+
+
+  additem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+  }
+
+  render() {
+    return (
+        <div className="App">
+            <header>
+            <form id="to-do-form" onSubmit={this.addItem}>
+              <input type="text" placeholder="Enter Text" value={this.state.currentItem.text} onChange={this.handleInput}/>
+              <button type="submit">Add</button>
+            </form>
+            </header>
+        </div>
+      );
+  }
+}
+
+export default App;
