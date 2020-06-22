b707d2f93fbbf90eaff9224959aea51a833a283a
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-06-22 09:29:09 +0530

    Updates

---
+++ precache-manifest.9001b5c0666cc7de263b2af3a1290fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9d2dccc1f90754f9974a381815f70b",
+    "url": "/react-todo-list-app/index.html"
+  },
+  {
+    "revision": "75a44575c64faec9d481",
+    "url": "/react-todo-list-app/static/css/main.ccd79635.chunk.css"
+  },
+  {
+    "revision": "65c9d7050c87d44dfab6",
+    "url": "/react-todo-list-app/static/js/2.38112f74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list-app/static/js/2.38112f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a44575c64faec9d481",
+    "url": "/react-todo-list-app/static/js/main.cc7a8fa9.chunk.js"
+  },
+  {
+    "revision": "ab408fb55fb02db4b236",
+    "url": "/react-todo-list-app/static/js/runtime-main.917f8b18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
